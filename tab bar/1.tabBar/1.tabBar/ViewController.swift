30911eb6ac87e9b9fd1fd188224a7ff05ce5feec
appledevelopment12 <128469441+appledevelopment12@users.noreply.github.com>
2023-03-21 21:51:17 +0530

    Add files via upload

---
+++ tab bar/1.tabBar/1.tabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1.tabBar
+//
+//  Created by Rajeev on 17/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ tab bar/1.tabBar/1.tabBar/myViewController.swift
@@ -0,0 +1,18 @@
+//
+//  myViewController.swift
+//  1.tabBar
+//
+//  Created by Rajeev on 17/03/23.
+//
+
+import UIKit
+
+class myViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
